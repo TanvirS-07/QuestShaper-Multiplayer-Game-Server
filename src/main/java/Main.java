224b04b976b25a1a50cf788e9e9a7b676a81ec3a
Tanvir <tanvirsingh382@gmail.com>
2026-03-19 16:51:36 +1100

    Setup Java HTTP server skeleton
    Updated main.java to a basic HTTP server skeleton like the week2 workshop one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,33 @@
-// src/main/java/Main.java
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        server.createContext("/move", exchange -> {
+            String response = "Move endpoint working";
+            sendResponse(exchange, response);
+        });
+
+        server.createContext("/info", exchange -> {
+            String response = "Info endpoint working";
+            sendResponse(exchange, response);
+        });
+
+        server.start();
+        System.out.println("Server started on port 8000");
+    }
+
+    private static void sendResponse(HttpExchange exchange, String response) throws IOException {
+        exchange.sendResponseHeaders(200, response.length());
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
